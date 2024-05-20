e1d59554bee47f941915960b261ad755412c3d31
Herbert Tamayo <hftamayo@gmail.com>
2024-05-20 13:18:17 -0600

    changing the approach about how to establish a ssh gateway

--- freeform/oj/jbridge.sh
+++ freeform/oj/jbridge.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh -L 30200:$(hostname -I | awk '{print $1}'):30200 tamayo@10.2.0.238
+#ssh -L 30200:$(hostname -I | awk '{print $1}'):30200 tamayo@10.2.0.238
+ssh -L 30200:localhost:30200 tamayo@10.2.0.238
